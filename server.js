4759e3a81d745c52a1724df4cc14d134a599c582
CivilianZero <Civilian.Protocol@gmail.com>
2016-11-22 11:10:07 -0500

    removed server junk

--- server.js
+++ server.js
@@ -17,7 +17,5 @@ app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/lib'));
-// Remove this when we start building our Sass
-app.use(express.static(__dirname + '/src/css'));
 
 app.listen(port);
